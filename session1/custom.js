ccdc8770685c46a057602edb4e921b703a68751d
marwa Radwan <marwaradwan872@hotmail.com>
2021-01-31 11:05:54 +0200

    s1

---
+++ session1/custom.js
@@ -0,0 +1,193 @@
+/*
+bank => customers => add customer , show all customers , edit balance(add, withdraw)
+generate accont number
+delete customer
+*/
+
+//variable
+// x = 2
+// x ="m"
+// x=true
+// x = null
+// x= [1,2,3]
+// x={
+//     name:'a',
+//     age:20
+// }
+// x=function(){
+//     console.log('hi')
+//}
+/*
+                                    any
+union               primtive                        object                  intersection
+                    -Numbers=> enum                 -class
+                    -bigint                         -interface
+                    -string                         -function
+                    -symbol                         - constructor
+                    - boolean                          -array => tuple
+
+                                    void
+                                    null
+                                    undefined
+*/
+// fname = "mazen"
+// lname = "mohammed"
+// console.log(fname,' ', lname)
+
+
+// `${fname} ${lname} ${5+10}`
+
+// var x = 5
+
+// function a(){
+//     console.log(x)
+// }
+// function b(){
+//     let x=3
+//     y=5
+//     if(true){
+//         let x=7
+//         console.log(`inner x ${x}`)
+//     }
+//     console.log(x)
+// }
+//a() //5
+//b() //3
+//console.log(`${x}-${y}`) 
+
+// function add1(x,y){
+//     return x+y
+// }
+
+// let add = function ( x, y=0 ) {
+//     //console.log(y)
+//     return x+y
+// }
+
+// console.log(add(2))
+
+
+// function getPrecentage(number, percent=10){
+//     arguments.length==1?number*10%:number*percent%
+// }
+
+
+// function getPercentage(num, percentage) {
+//     if (percentage) {
+//         return num * percentage
+//     }
+//     return "something went wrong"
+// }
+
+// console.log(getPercentage(10,0.5))
+
+// let getPercentage = function(n, p=10){
+//     return n*p/100
+// }
+// getPercentage(50)
+
+// let x = () => console.log('hello')
+
+// let x1 = () => {
+//     console.log('hello')
+// }
+
+// let user = {
+//     name:"mazen",
+//     yOfBirth:1980,
+//     job:"doctor",
+//     getAge: function(year){
+//         console.log(this.yOfBirth)
+//         return year-this.yOfBirth
+//     }
+// }
+// let getData=function(x){
+//     console.log(`${x.name} - ${x.age}`)
+// }
+// getData(user)
+// console.log(user.getAge(2021))
+// function test(){
+//     user.name = "ahmed"
+// }
+// test()
+// console.log(user)
+
+// account ={
+//     name, income, taxes
+// }
+// add income
+// reset account data
+// show account
+
+
+account = {
+    name :"ahmed",
+    income: 0,
+    taxes: 0
+}
+let addIncome = function(amount){
+    account.income += amount
+    account.taxes += (amount*0.1)
+}
+let resetAccount = function(){
+    account.income=0
+    account.taxes=0
+}
+let showAccount = function() {
+ console.log(`
+ account user is ${account.name}
+ income = ${account.income}
+ taxes = ${account.taxes}
+ `)   
+}
+addIncome(500)
+showAccount()
+addIncome(500)
+showAccount()
+resetAccount()
+showAccount()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
