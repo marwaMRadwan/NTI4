9f0f9f3ad82bf2d101f7d29e9c935a969194bbd2
marwa Radwan <marwaradwan872@hotmail.com>
2021-02-08 09:54:11 +0200

    s7

---
+++ session7/src/app.js
@@ -0,0 +1,19 @@
+const path=require('path')
+const express = require('express')
+const hbs = require('hbs')
+const data = require('./utils/data')
+
+const app = express()
+const PORT = 3000
+
+const myPublicFiles = path.join(__dirname, '../public')
+const myViewsFiles = path.join(__dirname, '../frontend/views')
+const myPartialsFiles = path.join(__dirname, '../frontend/layouts')
+
+app.set('view engine', 'hbs')
+app.set('views', myViewsFiles)
+hbs.registerPartials(myPartialsFiles)
+app.use(express.static(myPublicFiles))
+
+
+app.listen(PORT)
